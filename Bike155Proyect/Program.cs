c7483bcc85d0cd27f3100559e8ea0fcfde5df13c
Teo <mateo.herrera@udla.edu.ec>
2025-05-25 23:00:45 -0500

    a

--- Bike155Proyect/Program.cs
+++ Bike155Proyect/Program.cs
@@ -1,19 +1,28 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddDbContext<BikeBdMateoOrtegaHerrera>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("BikeBdMateoOrtegaHerrera") ?? throw new InvalidOperationException("Connection string 'BikeBdMateoOrtegaHerrera' not found.")));
 
-// Add services to the container.
+// Habilita CORS para llamadas desde MAUI
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -21,6 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors(); // ¡Importante! Habilita CORS aquí
 
 app.UseAuthorization();
 
